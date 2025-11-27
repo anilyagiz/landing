2abe520d5763de88145ec567fcdc4d3d5ace690a
anilyagiz <anill.yagiz@gmail.com>
2025-11-27 15:04:39 +0300

    g

--- pcweb/views/bottom_section/bottom_section.py
+++ pcweb/views/bottom_section/bottom_section.py
@@ -1,6 +1,6 @@
 import reflex as rx
 
-from pcweb.views.bottom_section.get_started import get_started
+
 from pcweb.views.bottom_section.newsletter import newsletter
 
 
@@ -8,6 +8,5 @@ from pcweb.views.bottom_section.newsletter import newsletter
 def bottom_section() -> rx.Component:
     return rx.box(
         newsletter(),
-        get_started(),
         class_name="flex flex-col items-center gap-20 lg:gap-32 pt-8 lg:pt-[6.5rem] w-[22rem] lg:w-[25rem]",
     )
